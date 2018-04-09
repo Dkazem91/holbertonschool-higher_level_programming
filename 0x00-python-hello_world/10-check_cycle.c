982d167564c3dc5444301ff54070151681a16b3c
Dkazem91 <Dkazemian@gmail.com>
2018-04-09 22:16:53 +0000

    added stuff for 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+	listint_t *first, *second;
+
+	first = list;
+	second = list;
+	while(first != NULL && second->next != NULL && second != NULL)
+	{
+		first = first->next;
+		second = second->next->next;
+
+		if(first == second)
+			return (1);
+	}
+	return (0);
+}
