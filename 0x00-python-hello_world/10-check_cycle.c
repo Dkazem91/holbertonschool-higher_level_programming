f5d0265d0230c938f66e607877dddc198f596678
Dkazem91 <Dkazemian@gmail.com>
2018-04-09 22:18:59 +0000

    fixed betty on 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,19 +1,23 @@
+#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "lists.h"
-
+/**
+ * check_cycle - checks for cycles in loop
+ * @list: list to take in
+ * Return: integer value
+ */
 int check_cycle(listint_t *list)
 {
 	listint_t *first, *second;
 
 	first = list;
 	second = list;
-	while(first != NULL && second->next != NULL && second != NULL)
+	while (first != NULL && second->next != NULL && second != NULL)
 	{
 		first = first->next;
 		second = second->next->next;
 
-		if(first == second)
+		if (first == second)
 			return (1);
 	}
 	return (0);
