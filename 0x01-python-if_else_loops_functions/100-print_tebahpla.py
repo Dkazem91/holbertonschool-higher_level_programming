b5990432830b6f0a5d321d097a49b57c89e39b05
Dkazem91 <Dkazemian@gmail.com>
2018-04-10 22:20:56 +0000

    fixed 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for a in range(122, 96, -1):
-    uppercase = 0
     if(a % 2 != 0):
-        uppercase = 32
-    print("{}".format(chr(a - uppercase)), end='')
+        a -= 32
+    print("{}".format(chr(a)), end='')
