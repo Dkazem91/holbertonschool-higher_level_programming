6c0f318cba87f104aebd52568367673c393f71db
Dkazem91 <Dkazemian@gmail.com>
2018-04-10 22:16:59 +0000

    fixed 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for a in "zYxWvUtSrQpOnMlKjIhGfEdCbA":
-    print("{}".format(a), end='')
+for a in range(122,96,-1):
+    uppercase = 0
+    if(a % 2 != 0):
+        uppercase = 32
+    print("{}".format(chr(a- uppercase)), end='')
