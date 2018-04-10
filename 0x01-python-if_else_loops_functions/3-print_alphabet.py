ff83c92b5954d1a13d7102bc7751d096428c81d5
Dkazem91 <Dkazemian@gmail.com>
2018-04-10 20:08:10 +0000

    fixed format i think

--- 0x01-python-if_else_loops_functions/3-print_alphabet.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-for a in "abcdefghijklmnopqrstuvwxyz":
-    if(a not in "qe"):
-        print(a, end='')
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for a in "abcdefghijklmnopqrstuvwxyz":
     if(a not in "qe"):
-        print(a, end='')
+        print("{}".format(a), end='')
