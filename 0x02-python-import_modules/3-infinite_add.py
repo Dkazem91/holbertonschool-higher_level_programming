895ca19ee3b078f1df468810c31b8320c064ddc4
Dkazem91 <Dkazemian@gmail.com>
2018-04-12 21:28:44 +0000

    fixed whitespace

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    result = sum(int(sys.argv[x]) for x in range(1,len(sys.argv)))
+    result = sum(int(sys.argv[x]) for x in range(1, len(sys.argv)))
     print("{}".format(result))
