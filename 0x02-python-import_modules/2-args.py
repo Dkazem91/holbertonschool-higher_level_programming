2dfc8739dba23d9bcf30193d9695098c3b75dc5c
Dkazem91 <Dkazemian@gmail.com>
2018-04-12 21:18:15 +0000

    fixed indent

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    args = len(sys.argv)
-    if(args <= 1):
-        print("{} arguments.".format(args -1))
-    elif(args == 2):
-        print("{} argument:".format(args - 1))
-    else:
-        print("{} arguments:".format(args - 1))
-        for x in range(1,args):
-            print("{}: {}".format(x, sys.argv[x]))
+import sys
+args = len(sys.argv)
+if(args <= 1):
+    print("{} arguments.".format(args - 1))
+elif(args == 2):
+    print("{} argument:".format(args - 1))
+else:
+    print("{} arguments:".format(args - 1))
+    for x in range(1, args):
+        print("{}: {}".format(x, sys.argv[x]))
