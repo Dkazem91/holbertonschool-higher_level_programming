134e528045479a333e26d37129eb44b095c32d61
Dkazem91 <Dkazemian@gmail.com>
2018-04-12 22:38:56 +0000

    magic advanced

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+
+    if(a < b):
+        c = (add(a,b))
+        for i in range(4,6):
+            c = add(c,i)
+        return c
+    else:
+        return sub(a,b)
+
+if __name__ == "__main__":
+    import dis
+    dis.dis(magic_calculation)
