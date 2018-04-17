42b1f4df9eb52e6eacaf4dc7efd66dd67c3a2e96
Dkazem91 <Dkazemian@gmail.com>
2018-04-17 20:49:01 +0000

    trying to fix 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,11 @@
 #!/user/bin/python3
+
+
 def delete_at(my_list=[], idx=0):
     if(idx < 0 or idx >= len(my_list)):
         copyList = my_list.copy()
         return(copyList)
-    else:
-        del my_list[idx]
-        copyList = my_list.copy()
-        return(copyList)
+
+    del my_list[idx]
+    copyList = my_list.copy()
+    return(copyList)
