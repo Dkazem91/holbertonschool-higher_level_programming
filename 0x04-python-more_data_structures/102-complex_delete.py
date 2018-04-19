19ac68c7edeb0723f32c8ebd68e9b61423d9bab2
Dkazem91 <Dkazemian@gmail.com>
2018-04-19 23:19:17 +0000

    pep8ing

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     if(value):
-        for key,val in list(a_dictionary.items()):
+        for key, val in list(a_dictionary.items()):
             if val is value:
                 del a_dictionary[key]
         return(a_dictionary)
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,3 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 def uniq_add(my_list=[]):
     return(sum(set(my_list)))
