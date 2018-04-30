2b10acfc2e3772d240f6ed748561fcd31eb9521f
Dkazem91 <Dkazemian@gmail.com>
2018-04-30 13:24:16 +0000

    fixed 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    newList = []
+    newList = [list_length]
     for x in range(list_length):
         result = 0
         try:
