df30635527978b7e8fef711a3245663db762f43a
Dkazem91 <Dkazemian@gmail.com>
2018-04-30 14:31:24 +0000

    fixed 102

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     result = 0
-    for i in range(1,3):
+    for i in range(1, 3):
             try:
                 if(i > a):
                     raise Exception("Too far")
@@ -11,7 +11,3 @@ def magic_calculation(a, b):
                 result = a + b
                 break
     return(result)
-
-if __name__ =="__main__":
-    import dis
-    dis.dis(magic_calculation)
