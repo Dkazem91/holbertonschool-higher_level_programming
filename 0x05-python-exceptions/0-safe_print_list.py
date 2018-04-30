3e3a6695eda2d359f2d655a4340216e4df4c4654
Dkazem91 <Dkazemian@gmail.com>
2018-04-30 13:18:09 +0000

    fixed indents

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    printed = 0
     if(my_list):
-        printed = 0
         for i in range(x):
             try:
                 print("{}".format(my_list[i]), end='')
@@ -9,5 +9,5 @@ def safe_print_list(my_list=[], x=0):
             except IndexError:
                 print()
                 return(printed)
-        print()
-        return(printed)
+    print()
+    return(printed)
