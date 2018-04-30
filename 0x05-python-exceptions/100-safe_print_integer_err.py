56ec42c072851bda32d130be525bb0924d16555e
Dkazem91 <Dkazemian@gmail.com>
2018-04-30 13:44:27 +0000

    fixed 100?

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,7 +3,7 @@ def safe_print_integer_err(value):
     import sys
     try:
         print("{:d}".format(value))
-        return(True)
     except (ValueError, TypeError) as err:
-        sys.stderr.write("Exception: {}".format(err))
+        sys.stderr.write("Exception: {}\n".format(err))
         return(False)
+    return(True)
