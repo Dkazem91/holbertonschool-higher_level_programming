120cf43f12c91d8773277c89d7571516bc3a8a43
Dkazem91 <Dkazemian@gmail.com>
2018-04-30 13:11:13 +0000

    fixed 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -12,12 +12,3 @@ def safe_print_list(my_list=[], x=0):
                 printed += 1
         print()
         return(printed)
-
-my_list = ["hello"]
-
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
