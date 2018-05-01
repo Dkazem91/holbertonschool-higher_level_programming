86ba409acb8dbef035f9f3fc8b3576cde8468f2a
Dkazem91 <Dkazemian@gmail.com>
2018-05-01 18:32:47 +0000

    Fixed pep8?

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -26,10 +26,9 @@ class Square:
         """
     @position.setter
     def position(self, value):
-        if(type(value) is not tuple
-           or len(value) is not 2
-           or type(value[0]) is not int
-           or type(value[1]) is not int):
+        if(type(value) is not tuple or len(value) is not 2 or
+           type(value[0]) is not int or
+           type(value[1]) is not int):
             raise TypeError("position must be a tuple of 2 positive integers")
         if(value[0] < 0 or value[1] < 0):
             raise ValueError("position must be a tuple of 2 positive integers")
