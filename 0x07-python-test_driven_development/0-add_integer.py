7774c0b8cac1a392d53f134c93b92eef94bccefd
Dkazem91 <Dkazemian@gmail.com>
2018-05-06 10:33:36 +0000

    fixed 0?

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-"""0-add_integer.py
+"""adds integer
 """
+
+
 def add_integer(a, b=98):
     """Returns a + b
-
     """
-    if not isinstance(a,(int,float)):
+    if(a == float('+inf') or a == float('-inf') or a != a
+       or not isinstance(a, (int, float))):
         raise TypeError("a must be an integer")
-    if not isinstance(b,(int,float)):
+
+    if(b == float('+inf') or b == float('-inf') or b != b
+       or not isinstance(b, (int, float))):
         raise TypeError("b must be an integer")
+
     return int(a) + int(b)
+print(add_integer(0))
