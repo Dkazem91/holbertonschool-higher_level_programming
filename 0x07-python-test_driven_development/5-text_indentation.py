47ddfc5e2b8ef5bdf76c8de02ae4d8929d3db715
Dkazem91 <Dkazemian@gmail.com>
2018-05-06 11:18:16 +0000

    fixed 5 pep8 and tests pass

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def text_indentation(text):
-    if not isinstance(text,str):
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
     beg = 0
-    for idx,val in enumerate(text):
+    for idx, val in enumerate(text):
         if val in '?:.':
             print(text[beg:idx + 1].strip() + '\n')
             beg = idx + 1
     if not beg:
-        print(text,end='')
+        print(text, end='')
     elif beg is not len(text):
-        print(text[beg:idx + 1].strip(),end='')
+        print(text[beg:idx + 1].strip(), end='')
