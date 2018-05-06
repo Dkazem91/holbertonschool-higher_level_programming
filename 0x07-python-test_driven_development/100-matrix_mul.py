81cff5440863406ee0502a44a51f3888c843dd97
Dkazem91 <Dkazemian@gmail.com>
2018-05-06 13:21:28 +0000

    fixed error message

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -5,9 +5,9 @@ def matrix_mul(m_a, m_b):
     if not isinstance(m_b,list):
         raise TypeError('m_b must be a list')
     if not len(m_a) or 0 in [len(listx) for listx in m_a]:
-        raise TypeError('m_a can\'t be empty')
+        raise TypeError("m_a can't be empty")
     if not len(m_b) or 0 in [len(listx) for listx in m_b]:
-        raise TypeError('m_b can\'t be empty')
+        raise TypeError("m_b can't be empty")
     if any(False in x for x in
            [[isinstance(ele,(int,float)) for ele in listx] for listx in m_a]):
             raise TypeError('m_a should contain only integers or floats')
@@ -21,3 +21,4 @@ def matrix_mul(m_a, m_b):
     if len(m_a) != len([list(x) for x in zip(*m_b)]):
         raise ValueError('m_a and m_b can\'t be multiplied')
     return([[sum(a*b for a,b in zip(colA,colB)) for colB in zip(*m_b)] for colA in m_a])
+print(matrix_mul([[5,6],[7,8]],[[]]))
