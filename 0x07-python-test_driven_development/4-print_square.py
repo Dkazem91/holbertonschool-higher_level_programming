e395e4d29d46f4257c21fdcfc214ca98e2869490
Dkazem91 <Dkazemian@gmail.com>
2018-05-06 11:10:42 +0000

    fixed square?

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -4,11 +4,9 @@
 
 def print_square(size):
     if size is 0:
-        print()
         return
     if not isinstance(size, int):
         raise TypeError('size must be an integer')
     if size < 0:
         raise ValueError('size must be >= 0')
     print('\n'.join('#' * size for x in range(size)))
-print_square(0)
