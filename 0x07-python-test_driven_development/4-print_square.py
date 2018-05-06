d8f9478388373210a7f885d9b0f2f02d35cadd99
Dkazem91 <Dkazemian@gmail.com>
2018-05-06 11:09:41 +0000

    fixed 0 case i hope

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 '''print square module'''
+
+
 def print_square(size):
-    if not isinstance(size,int):
+    if size is 0:
+        print()
+        return
+    if not isinstance(size, int):
         raise TypeError('size must be an integer')
     if size < 0:
         raise ValueError('size must be >= 0')
     print('\n'.join('#' * size for x in range(size)))
+print_square(0)
