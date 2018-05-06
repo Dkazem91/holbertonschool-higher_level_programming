409904aa8b9309e821815c180c709ce2eed09532
Dkazem91 <Dkazemian@gmail.com>
2018-05-06 14:29:20 +0000

    added error messages

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -2,16 +2,16 @@
 def matrix_mul(m_a, m_b):
     if not isinstance(m_a, list):
         raise TypeError('m_a must be a list')
+    elif any(False in [isinstance(listx,list) for listx in m_a]:
+        raise TypeError("m_a must be a list of lists")
     if not isinstance(m_b, list):
         raise TypeError('m_b must be a list')
-    if not len(m_a):
-        raise ValueError("m_a can't be empty")
-    elif 0 in [len(listx) for listx in m_a]:
-        raise TypeError("m_a must be a list of lists")
-    if not len(m_b):
-        raise ValueError("m_b can't be empty")
-    elif 0 in [len(listx) for listx in m_b]:
+    elif any(False in [isinstance(listx,list) for listx in m_b]:
         raise TypeError("m_b must be a list of lists")
+    if not len(m_a) or 0 in [len(listx) for x in m_a]:
+        raise ValueError("m_a can\'t be empty")
+    if not len(m_b) or 0 in [len(listx) for x in m_b]:
+        raise ValueError("m_b can\'t be empty")
     if any(False in x for x in
            [[isinstance(ele, (int, float))
              for ele in listx] for listx in m_a]):
@@ -21,10 +21,10 @@ def matrix_mul(m_a, m_b):
              for ele in listx] for listx in m_b]):
             raise TypeError('m_b should contain only integers or floats')
     if len(set(len(listx) for listx in m_a)) > 1:
-        raise TypeError("each row of m_a must should be of the same size")
+             raise TypeError('each row of m_a must should be of the same size')
     if len(set(len(listx) for listx in m_b)) > 1:
-        raise TypeError("each row of m_b must should be of the same size")
+             raise TypeError('each row of m_b must should be of the same size')
     if len(m_a) != len([list(x) for x in zip(*m_b)]):
-        raise ValueError("m_a and m_b can't be multiplied")
+             raise ValueError('m_a and m_b can\'t be multiplied')
     return([[sum(a*b for a, b in
                  zip(colA, colB)) for colB in zip(*m_b)] for colA in m_a])
