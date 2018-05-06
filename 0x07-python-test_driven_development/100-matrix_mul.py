2b0dd66570f7f701f57ac85ead6c541c136c604d
Dkazem91 <Dkazemian@gmail.com>
2018-05-06 14:57:23 +0000

    fixed indents, errors, visuals

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -2,26 +2,22 @@
 def matrix_mul(m_a, m_b):
     if not isinstance(m_a, list):
         raise TypeError('m_a must be a list')
-    if any(False in xfor x in
-           [isinstance(listx, list) for listx in m_a]):
+    if False in [isinstance(listx, list) for listx in m_a]:
         raise TypeError("m_a must be a list of lists")
     if not isinstance(m_b, list):
         raise TypeError('m_b must be a list')
-    if any(False in x for x in
-           [isinstance(listx, list) for listx in m_b]):
+    if False in [isinstance(listx, list) for listx in m_b]:
         raise TypeError("m_b must be a list of lists")
-    if not len(m_a) or 0 in [len(listx) for x in m_a]:
+    if not len(m_a) or 0 in [len(listx) for listx in m_a]:
         raise ValueError("m_a can\'t be empty")
-    if not len(m_b) or 0 in [len(listx) for x in m_b]:
+    if not len(m_b) or 0 in [len(listx) for listx in m_b]:
         raise ValueError("m_b can\'t be empty")
-    if any(False in x for x in
-           [[isinstance(ele, (int, float))
-             for ele in listx] for listx in m_a]):
-            raise TypeError('m_a should contain only integers or floats')
-    if any(False in x for x in
-           [[isinstance(ele, (int, float))
-             for ele in listx] for listx in m_b]):
-            raise TypeError('m_b should contain only integers or floats')
+    if any(False in x for x in [[isinstance(ele, (int, float))
+                                 for ele in listx] for listx in m_a]):
+        raise TypeError('m_a should contain only integers or floats')
+    if any(False in x for x in [[isinstance(ele, (int, float))
+                                 for ele in listx] for listx in m_b]):
+        raise TypeError('m_b should contain only integers or floats')
     if len(set(len(listx) for listx in m_a)) > 1:
         raise TypeError('each row of m_a must should be of the same size')
     if len(set(len(listx) for listx in m_b)) > 1:
