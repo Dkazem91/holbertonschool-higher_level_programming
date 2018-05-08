525a5b2cfbc5d50511bf7c54b689a128de285b2a
Dkazem91 <Dkazemian@gmail.com>
2018-05-08 10:19:27 +0000

    fixed bigger or equal

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -78,8 +78,8 @@ class Rectangle:
         if not isinstance(rect_2, Rectangle):
             raise TypeError('rect_2 must be an instance of Rectangle')
         if rect_1.area() >= rect_2.area():
-            return rect_1.area
-        return rect_2.area
+            return rect_1
+        return rect_2
 
     def __str__(self):
         """modifies str object
