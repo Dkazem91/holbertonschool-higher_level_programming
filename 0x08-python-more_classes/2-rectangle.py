4b8f7f59ba9efb2fa404c5712280a422495494f2
Dkazem91 <Dkazemian@gmail.com>
2018-05-08 10:26:06 +0000

    Fixed perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -59,4 +59,6 @@ class Rectangle:
         """ returns perimeter
 
         """
+        if self.height == 0 or self.width == 0:
+            return 0
         return (self.height * 2) + (self.width * 2)
