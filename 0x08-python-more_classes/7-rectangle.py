caba804a81de284ae7d660e31f9f6bc7b63631b5
Dkazem91 <Dkazemian@gmail.com>
2018-05-08 10:22:19 +0000

    fixed pep8

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -75,8 +75,8 @@ class Rectangle:
         """
         if not self.perimeter:
             return ""
-        return('\n'.join("{}".format(self.print_symbol)
-                         * self.width for x in range(self.height)))
+        return('\n'.join("{}".format(
+            self.print_symbol) * self.width for x in range(self.height)))
 
     def __repr__(self):
         """modifies repr object
