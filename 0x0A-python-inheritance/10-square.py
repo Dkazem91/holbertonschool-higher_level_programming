df33e6470f1ae35886e25c4c2adac37e493227bb
Dkazem91 <Dkazemian@gmail.com>
2018-05-16 09:57:22 +0000

    fixed 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,43 +1,7 @@
 #!/usr/bin/python3
 """basegeometryclass"""
 
-
-class BaseGeometry:
-
-    def area(self):
-        """raises exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates value for integer and positive"""
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-"""class rectangle"""
-
-
-class Rectangle(BaseGeometry):
-    """creates rectangle class"""
-    def __init__(self, width, height):
-        """initializes rectangle"""
-        if not super().integer_validator("width", width):
-            self.__width = width
-        if not super().integer_validator("height", height):
-            self.__height = height
-
-    def area(self):
-        """returns area"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """returns string"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
-
-"""class square"""
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
@@ -45,6 +9,6 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """initializes rectangle"""
-        self.integer_validator("size",size)
+        self.integer_validator("size", size)
         self.__size = size
-        super().__init__(size,size)
+        super().__init__(size, size)
