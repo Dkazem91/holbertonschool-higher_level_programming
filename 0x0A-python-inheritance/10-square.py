6b60714b83205ef8fc866e51a01167186553eee4
Dkazem91 <Dkazemian@gmail.com>
2018-05-16 09:55:05 +0000

    fixed validator

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -45,5 +45,6 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """initializes rectangle"""
-        Rectangle.__init__(self, size, size)
+        self.integer_validator("size",size)
         self.__size = size
+        super().__init__(size,size)
