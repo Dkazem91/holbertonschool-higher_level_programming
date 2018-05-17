6209a45ac3ae6567d45354c5614a8e6969dad597
Dkazem91 <Dkazemian@gmail.com>
2018-05-17 10:17:27 +0000

    fixed 12?

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -12,8 +12,7 @@ class Student:
 
     def to_json(self, attrs=None):
         """prints __dict__"""
-        if(attrs and isinstance(attrs, list) and
-           all(isinstance(x, str) for x in attrs)):
+        if(isinstance(attrs, list) and all(isinstance(x, str) for x in attrs)):
             return({x: y for x, y in self.__dict__.items() if x in attrs})
         else:
             return self.__dict__
