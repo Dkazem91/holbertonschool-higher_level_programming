50ce0827a8d8a44d8527714351257f88a2698984
Dkazem91 <Dkazemian@gmail.com>
2018-05-17 10:06:50 +0000

    fixed 14

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -4,15 +4,17 @@
 
 def pascal_triangle(n):
     """does it"""
+    bullshit = []
     for i in range(n):
         if i is 0:
-            print([1])
+            bullshit.append([1])
             continue
         if i is 1:
             oldList = [1, 1]
-            print(oldList)
+            bullshit.append(oldList)
             continue
         adder = [1] + [sum(oldList[x:x + 2]) for x in range(len(oldList) - 1)]
         newList = adder + [1]
-        print(newList)
+        bullshit.append(newList)
         oldList = newList
+    return bullshit
