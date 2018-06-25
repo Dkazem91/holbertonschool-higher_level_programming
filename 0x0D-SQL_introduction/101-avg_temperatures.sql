8bf6513919b38497eb0b46082d633c0ac713bb81
Dkazem91 <Dkazemian@gmail.com>
2018-06-25 18:02:09 +0000

    now it's just one line

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,2 @@
 -- average by city
-SELECT city, AVG(value) AS avg_tmp FROM temperatures
-GROUP BY city ORDER BY avg_tmp DESC;
+SELECT city, AVG(value) AS avg_tmp FROM temperatures GROUP BY city ORDER BY avg_tmp DESC;
