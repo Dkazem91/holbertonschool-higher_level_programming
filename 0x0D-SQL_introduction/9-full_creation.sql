93ec46c234e7922e681f1729d7191cb5b917a2c7
Dkazem91 <Dkazemian@gmail.com>
2018-06-25 17:01:43 +0000

    removed auto increment

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,11 +1,10 @@
 -- creates and populates table
 CREATE TABLE IF NOT EXISTS second_table (
-       id INT AUTO_INCREMENT,
+       id INT,
        name VARCHAR(256),
-        score INT,
-	PRIMARY KEY(id)
+        score INT
 );
 
-INSERT INTO second_table (name, score) VALUES
-       ("John", 10), ("Alex", 13), ("Bob", 14),
-       ("George", 8);
+INSERT INTO second_table (id,name, score) VALUES
+       (1,"John", 10), (2,"Alex", 13), (3,"Bob", 14),
+       (4,"George", 8);
