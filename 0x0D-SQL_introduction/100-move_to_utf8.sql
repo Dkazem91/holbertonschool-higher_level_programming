e78eaf09ac957a164bbdd586405d705c2e948d1c
Dkazem91 <Dkazemian@gmail.com>
2018-06-25 17:37:50 +0000

    fixed advanced text

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,9 @@
 -- alters stuff to utf8 and collates
 ALTER DATABASE hbtn_0c_0
 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
 ALTER TABLE first_table
-CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table
-MODIFY COLUMN name VARCHAR(256)
+MODIFY name VARCHAR(256)
 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
