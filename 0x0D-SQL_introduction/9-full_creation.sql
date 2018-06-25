ed60cd0f0ee65c1d9ad74adea64e212e0fa1fe79
Dkazem91 <Dkazemian@gmail.com>
2018-06-25 17:13:00 +0000

    fixed styling for 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,9 +1,8 @@
 -- creates and populates table
 CREATE TABLE IF NOT EXISTS second_table (
-       id INT,
-       name VARCHAR(256),
-       score INT
-);
-INSERT INTO second_table (id,name, score) VALUES
+       `id` INT,
+       `name` VARCHAR(256),
+       `score` INT);
+INSERT INTO second_table (`id`,`name`, `score`) VALUES
        (1,"John", 10), (2,"Alex", 13), (3,"Bob", 14),
        (4,"George", 8);
