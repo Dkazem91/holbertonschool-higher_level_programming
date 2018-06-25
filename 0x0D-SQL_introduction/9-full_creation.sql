5e78eb86cf733831b5edf849540dec2bcb855256
Dkazem91 <Dkazemian@gmail.com>
2018-06-25 17:11:38 +0000

    not sure

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,9 +2,8 @@
 CREATE TABLE IF NOT EXISTS second_table (
        id INT,
        name VARCHAR(256),
-        score INT
+       score INT
 );
-
 INSERT INTO second_table (id,name, score) VALUES
        (1,"John", 10), (2,"Alex", 13), (3,"Bob", 14),
        (4,"George", 8);
