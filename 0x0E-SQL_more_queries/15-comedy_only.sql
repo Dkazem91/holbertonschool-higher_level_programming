c44540f7e29bd4734506f4992b8c8d747bb27b1c
Dkazem91 <Dkazemian@gmail.com>
2018-06-26 23:04:02 +0000

    added 15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -3,6 +3,6 @@ SELECT ts.title FROM tv_shows AS ts
 JOIN tv_show_genres AS tgs
 ON ts.id = tgs.show_id
 JOIN tv_genres AS tgg
-ON tgs.show_id = tgs.show_id
-WHERE tgg.name='Comedy'
-GROUP BY tgg.name ASC;
+ON tgs.genre_id = tgg.id
+WHERE tgg.name = 'Comedy'
+GROUP BY ts.title ASC;
