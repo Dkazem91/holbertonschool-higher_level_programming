b2b370fce5ea73202cc592970e9258975ace5b42
Dkazem91 <Dkazemian@gmail.com>
2018-06-26 23:18:37 +0000

    added 16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- only comedy
+SELECT ts.title, tgg.name FROM tv_shows AS ts
+LEFT JOIN tv_show_genres AS tgs
+ON ts.id = tgs.show_id
+LEFT JOIN tv_genres AS tgg
+ON tgs.genre_id = tgg.id
+GROUP BY ts.title ASC, tgg.name ASC;
