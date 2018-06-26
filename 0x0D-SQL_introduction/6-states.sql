79141403cbc3ea592b43bb40017188abb656d425
Dkazem91 <Dkazemian@gmail.com>
2018-06-26 12:46:40 +0000

    added states

---
+++ 0x0D-SQL_introduction/6-states.sql
@@ -0,0 +1,5 @@
+-- creates database and table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states
+(`id` INT AUTO_INCREMENT, `name` VARCHAR(256) NOT NULL, PRIMARY KEY(id));
