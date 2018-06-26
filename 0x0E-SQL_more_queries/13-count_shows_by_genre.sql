f446a39633d5e1f303a557f80955d5eb34aab552
Dkazem91 <Dkazemian@gmail.com>
2018-06-26 22:08:16 +0000

    uppercased as

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,9 +1,9 @@
 -- genre id by show
-SELECT tgs.name, COUNT(tg.genre_id) as number_shows
+SELECT tgs.name, COUNT(tg.genre_id) AS number_shows
 FROM tv_shows AS ts
 LEFT JOIN tv_show_genres AS tg
 ON ts.id = tg.show_id
-LEFT JOIN tv_genres as tgs
+LEFT JOIN tv_genres AS tgs
 ON tg.genre_id = tgs.id
 WHERE tg.genre_id IS NOT NULL
 GROUP BY tgs.name ORDER BY number_shows DESC;
