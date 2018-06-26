dacc7707513bd61ac6f86c278d3ea4cd38687fd7
Dkazem91 <Dkazemian@gmail.com>
2018-06-26 19:28:58 +0000

    up to3

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,3 @@
+-- forces a name
+CREATE TABLE IF NOT EXISTS `force_name`
+(`id` INT, `name` VARCHAR(256) NOT NULL);
---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,3 @@
+-- forces a name
+CREATE TABLE IF NOT EXISTS `id_not_null`
+(`id` INT DEFAULT 1, `name` VARCHAR(256));
