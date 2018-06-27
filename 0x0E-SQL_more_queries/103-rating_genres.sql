142606d8633789943777c831d76066341cba2543
Dkazem91 <Dkazemian@gmail.com>
2018-06-27 02:26:18 +0000

    103 added

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,7 @@
+-- This list all tv show ratings
+SELECT tg.name, SUM(tr.rate) AS rating FROM tv_show_genres tgs
+JOIN tv_genres tg
+ON tg.id = tgs.genre_id
+JOIN tv_show_ratings tr
+ON tr.show_id = tgs.show_id
+GROUP BY  tgs.genre_id DESC ORDER BY RATING DESC;
