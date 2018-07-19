e507e1ef69019a053544e84b6d7974287bdec92b
Dkazem91 <Dkazemian@gmail.com>
2018-07-19 21:36:33 +0000

    testing purposes

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -9,15 +9,14 @@ import sys
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Session = sessionmaker()
     session = Session(bind=engine)
     Base.metadata.create_all(engine)
-    state = session.query(State).filter(
-        State.name == sys.argv[4]).all()
-    if state:
-        for place in state:
-            print("{}".format(place.id))
+    states = session.query(State).filter(State.name == argv[4]).all()
+    if states:
+        for state in states:
+            print("{}".format(state.id))
     else:
-        print("Not Found")
+        print("Not found")
     session.close()
