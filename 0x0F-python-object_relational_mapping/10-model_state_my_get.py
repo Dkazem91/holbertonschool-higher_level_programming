9ad1b17677d85c2124dc951952f2563bf85c7bf5
Dkazem91 <Dkazemian@gmail.com>
2018-07-19 21:37:18 +0000

    added 10 sys

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -9,11 +9,11 @@ import sys
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker()
     session = Session(bind=engine)
     Base.metadata.create_all(engine)
-    states = session.query(State).filter(State.name == argv[4]).all()
+    states = session.query(State).filter(State.name == sys.argv[4]).all()
     if states:
         for state in states:
             print("{}".format(state.id))
