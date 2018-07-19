71e0e17fc6b6486e24ec74d13ee4ef310455baea
Dkazem91 <Dkazemian@gmail.com>
2018-07-19 20:39:32 +0000

    commented

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-#sql alchemy awesome
+# sql alchemy awesome
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
+    # creates state
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
+
+
 def __repr__(self):
+    # reaper
     return "<States(id='%s', name='%s')>" % (self.id, self.name)
