a1a9ae972cd9ad7e05b83095f10f1a66d147b927
Dkazem91 <Dkazemian@gmail.com>
2018-07-19 21:31:08 +0000

    not sure

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -5,12 +5,14 @@ from sqlalchemy import MetaData
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
+
+
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker()
+    session = Session(bind=engine)
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
     state = session.query(State).filter(
         State.name == sys.argv[4]).all()
     if state:
