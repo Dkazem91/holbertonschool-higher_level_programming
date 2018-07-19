dca5b11437fd12294b72d839c7f0face7a99c678
Dkazem91 <Dkazemian@gmail.com>
2018-07-19 22:34:57 +0000

    removed relationship :(

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -13,4 +13,3 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    city = relationship("City", back_populates="state")
