592c1c5cdf21cfb5e673b9861a753e28d913e87b
Dkazem91 <Dkazemian@gmail.com>
2018-08-06 17:45:03 +0000

    added 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# posts email
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    data = {'email': sys.argv[2]}
+    data = bytes(urllib.parse.urlencode(data).encode())
+    handler = urllib.request.urlopen(sys.argv[1], data)
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.read().decode('utf-8'))
