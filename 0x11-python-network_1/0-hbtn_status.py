0307116bb15d0b5c68d604fcbf2bb7a838a801c2
Dkazem91 <Dkazemian@gmail.com>
2018-08-06 17:13:20 +0000

    added 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# fetches website
+import urllib.request
+
+with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+    content = response.read()
+    url = content.decode('utf-8')
+    typed = response.info()
+    print("Body response:")
+    print("\t - type: {}".format(type(content)))
+    print("\t - content: {}".format(content))
+    print("\t - utf8 content: {}".format(url))
