1b0c3940be61ad8142da43bfb1e46eef9819c7d4
Dkazem91 <Dkazemian@gmail.com>
2018-08-06 17:49:26 +0000

    added handler to urlopen

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     data = {'email': sys.argv[2]}
     data = bytes(urllib.parse.urlencode(data).encode())
     handler = urllib.request.urlopen(sys.argv[1], data)
-    with urllib.request.urlopen(sys.argv[1]) as response:
+    with urllib.request.urlopen(handler) as response:
         print(response.read().decode('utf-8'))
