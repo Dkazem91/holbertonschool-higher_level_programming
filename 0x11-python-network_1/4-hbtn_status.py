9b6c8de862ae016c895738acf17f34301160a0fa
Dkazem91 <Dkazemian@gmail.com>
2018-08-06 18:08:40 +0000

    first push for 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# requests use
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
