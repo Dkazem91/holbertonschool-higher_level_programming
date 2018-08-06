8d1bd4c79385d7b19c3fe6053fd991b9dab1b0c1
Dkazem91 <Dkazemian@gmail.com>
2018-08-06 18:17:51 +0000

    fixed 6?

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,4 +5,4 @@ from sys import argv
 
 if __name__ == "__main__":
     r = requests.post(argv[1], data={'email': argv[2]})
-    print(requests.get(argv[1]).text)
+    print("Your email is: {}".format(requests.get(argv[1]).text))
