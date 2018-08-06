86f47d213baa1b6b877d0895b8b24ab98b6be0a2
Dkazem91 <Dkazemian@gmail.com>
2018-08-06 20:02:47 +0000

    added 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# requests use
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    if argv[1]:
+        qVar = argv[1]
+    else:
+        qVar = ""
+    r = requests.post("http://0.0.0.0:5000/search_user", data={'q': qVar})
+    try:
+        if r.json():
+            print("[{}] {}".format(r.json().get('id'), r.json().get('name')))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
