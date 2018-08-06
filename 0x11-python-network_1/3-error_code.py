fd9f03e5f0d04b12e7767020695d7fcb2711d631
Dkazem91 <Dkazemian@gmail.com>
2018-08-06 18:00:49 +0000

    added 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# posts email
+import urllib.request
+import urllib.parse
+import urllib.error
+import sys
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            thisPage = response.read().decode('utf-8')
+            print(thisPage)
+    except urllib.error.URLError as e:
+        print(e.reason)
