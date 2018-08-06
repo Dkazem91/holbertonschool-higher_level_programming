2f4e66ee91adb2da3f1f69083375b94f5b7da173
Dkazem91 <Dkazemian@gmail.com>
2018-08-06 19:41:00 +0000

    added 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# requests use error codes
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    r = requests.get(argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
