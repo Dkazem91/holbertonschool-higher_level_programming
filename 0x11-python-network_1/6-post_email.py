21c13139f0badf90b9b4dee31f529b316af59953
Dkazem91 <Dkazemian@gmail.com>
2018-08-06 18:16:31 +0000

    added 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# requests use
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    r = requests.post(argv[1], data={'email': argv[2]})
+    print(requests.get(argv[1]).text)
