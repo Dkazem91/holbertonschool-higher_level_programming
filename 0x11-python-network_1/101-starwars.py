30d64bd02307b517bc2a1b896bb13c60880588c1
Dkazem91 <Dkazemian@gmail.com>
2018-08-07 00:05:35 +0000

    paginated

---
+++ 0x11-python-network_1/101-starwars.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# requests use
+import requests
+from sys import argv
+
+
+def paginate(address):
+    if address is None:
+        return
+    r = requests.get(address).json()
+    for dik in r.get('results'):
+        print(dik.get('name'))
+    if r.get('next'):
+        return paginate(r.get('next'))
+
+
+if __name__ == "__main__":
+    r = requests.get("https://swapi.co/api/").json().get('people')
+    idsite = "{}?search={}".format(r, argv[1])
+    section = requests.get(idsite).json()
+    print("Number of results: {}".format(section.get('count')))
+    for dik in section.get('results'):
+        print(dik.get('name'))
+    if section.get('next'):
+        paginate(section.get('next'))
