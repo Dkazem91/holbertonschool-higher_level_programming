6a812911c5cea0f927ce5ee0e5b900825ae93c96
Dkazem91 <Dkazemian@gmail.com>
2018-08-28 20:08:30 +0000

    less than equal to 1

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 let nums = process.argv.slice(2).map(x => Number(x));
-if (nums.length === 1) {
+if (nums.length <= 1) {
   console.log(0);
 } else {
   console.log(nums.sort().reverse()[1]);
