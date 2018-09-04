e2e0b334bb8dfeb2e54aa7e70a14431ccbea92c4
Dkazem91 <Dkazemian@gmail.com>
2018-09-04 23:31:42 +0000

    added square inheritance

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const Square5 = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
+class Square extends Square5 {
   charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
