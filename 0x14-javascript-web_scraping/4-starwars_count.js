fd1dd9e049ada3f702d05ced9b3e1ab884696251
Dkazem91 <Dkazemian@gmail.com>
2018-09-05 19:39:37 +0000

    two loops

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
 const address = process.argv[2];
-const achil = 'https://swapi.co/api/people/18/';
 
 request(address, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    let results = JSON.parse(body)['results'];
+    let results = JSON.parse(body).results;
     let count = 0;
     for (let i in results) {
-      if (results[i]['characters'].indexOf(achil) >= 0) { count += 1; }
+      for (let chr of results[i].characters) {
+        if (chr.search('/18/') > 0) { count += 1; }
+      }
     }
     console.log(count);
   }
