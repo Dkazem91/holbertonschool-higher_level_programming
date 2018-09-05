1151e3c773d336ac3b6852714324eb699794567c
Dkazem91 <Dkazemian@gmail.com>
2018-09-05 19:47:30 +0000

    added search

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
 const address = process.argv[2];
-const achil = 'https://swapi.co/api/people/18/';
 
 request(address, function (error, response, body) {
   if (error) {
@@ -11,7 +10,7 @@ request(address, function (error, response, body) {
     let count = 0;
     for (let i in results) {
       for (let chr of results[i].characters) {
-        if (chr === achil) { count += 1; }
+        if (chr.search('/18/') > 0) { count += 1; }
       }
     }
     console.log(count);
