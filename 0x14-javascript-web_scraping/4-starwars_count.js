ca40d81dfaa2347fa34e6fb218a10d345501a269
Dkazem91 <Dkazemian@gmail.com>
2018-09-05 19:21:43 +0000

    added 4?

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-let request = require('request');
-let address = process.argv[2];
+const request = require('request');
+const address = process.argv[2];
 let achil = 'https://swapi.co/api/people/18/';
+
+
 request(address, function (error, response, body) {
   if (error) {
     console.log(error);
@@ -9,9 +11,7 @@ request(address, function (error, response, body) {
     let results = JSON.parse(body)['results'];
     let count = 0;
     for (let i in results) {
-      if (results[i]['characters'].indexOf(achil) >= 0) {
-        count += 1;
-      }
+      if (results[i]['characters'].indexOf(achil) >= 0) {count += 1;}
     }
     console.log(count);
   }
